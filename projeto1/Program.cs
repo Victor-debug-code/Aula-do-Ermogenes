782e975f6a1522d462dbbcedf2dbc688d6a38a86
Victor-debug-code <60802958+Victor-debug-code@users.noreply.github.com>
2020-03-13 20:26:17 -0300

    Add files via upload

---
+++ projeto1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String Nome = ("Victor");
+            String Sobrenome = ("Morais");
+            String nomeCompleto = String.Concat(Nome, " ", Sobrenome);
+            Console.WriteLine(Nome + " " + Sobrenome);
+
+
+            String Sobrenome1 = ("Morais,");
+            String Nome1 =("Victor");
+            String nomeCompleto1 = String.Concat(Sobrenome1, " ", Nome1);
+            Console.WriteLine(Sobrenome1 + " " + Nome1);
+        }
+    }
+}
