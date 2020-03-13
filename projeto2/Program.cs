b8de25d87a8bab61f1a0ceb6954e1ee9a920313c
Victor-debug-code <60802958+Victor-debug-code@users.noreply.github.com>
2020-03-13 20:38:17 -0300

    Add files via upload

---
+++ projeto2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string textoDigitado = Console.ReadLine();
+            Console.Write("Olá, ");
+            Console.Write(textoDigitado);
+            Console.WriteLine("!");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
