6655f4d2ced4ccdb97a90f19657d68793215578f
Victor-debug-code <60802958+Victor-debug-code@users.noreply.github.com>
2020-03-13 22:41:19 -0300

    Add files via upload

---
+++ projeto4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace projeto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor digite uma frase:");
+            String flase = Console.ReadLine();
+            String frase = flase.Replace("A","4")
+                                .Replace("a","4")
+                                .Replace("E","3")
+                                .Replace("e","3")
+                                .Replace("I","1")
+                                .Replace("i","1")
+                                .Replace("L","1")
+                                .Replace("l","1")
+                                .Replace("O","0")
+                                .Replace("o","0")
+                                .Replace("T","7")
+                                .Replace("t","7")
+                                .Replace("S","5")
+                                .Replace("s","5");
+            Console.WriteLine(frase); 
+                       
+        }
+    }
+}
