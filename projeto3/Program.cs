cb14b2947b5e733b17083bcc8385df5b94fbad50
Victor-debug-code <60802958+Victor-debug-code@users.noreply.github.com>
2020-03-13 22:06:16 -0300

    Add files via upload

---
+++ projeto3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace projeto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor digite uma frase:");
+            String frase = Console.ReadLine();
+            String flase = frase.Replace("r","l")
+                                .Replace("R","L");
+            Console.WriteLine(flase);                        
+        }
+    }
+}
